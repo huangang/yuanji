b9b06ab7397bce026b58b374937a4a1a47442c4c
huangang <huangangzhang@gmail.com>
2019-04-21 13:24:40 +0800

    update rand_image.dart

--- lib/page/daily_one.dart
+++ lib/page/daily_one.dart
@@ -24,7 +24,7 @@ class _DailyOneState extends State<DailyOne> with AutomaticKeepAliveClientMixin 
     await audioPlayer.play(kUrl);
   }
 
- @override
+  @override
   initState() { 
     super.initState();
   }
--- lib/page/rand_image.dart
+++ lib/page/rand_image.dart
@@ -1,11 +1,52 @@
 import 'package:flutter/material.dart';
+import '../util/api_utils.dart';
+import 'dart:convert';
 
-class RandImage extends StatelessWidget {
+
+class RandImage extends StatefulWidget {
+  @override
+  _RandImageState createState() => _RandImageState();
+}
+
+class _RandImageState extends State<RandImage> with AutomaticKeepAliveClientMixin {
+
+  @override
+  bool get wantKeepAlive => true;
+  String imageUrl = 'https://api.uomg.com/api/rand.img2';
+
+  Future _getRandImage() async{
+    final response = await ApiUtils.get('https://api.uomg.com/api/rand.img2?format=json');
+    final data = json.decode(response);
+    setState(() {
+      imageUrl = data['imgurl'];
+    });
+  }
+
+  @override
+  initState() { 
+    super.initState();
+    _getRandImage();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new Image.network(
-      'https://api.uomg.com/api/rand.img2',
-      fit: BoxFit.cover
+    return Scaffold(
+      floatingActionButtonLocation:  FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _getRandImage();
+        },
+        child: Icon(Icons.cached),
+        elevation: 0.0,
+        backgroundColor: Color.fromRGBO(58, 66, 86, 1.0),
+      ),
+      body: Center(
+        child: new Image.network(
+          imageUrl,
+          fit: BoxFit.cover,
+          height: MediaQuery.of(context).size.height
+        ),
+      )
     );
   }
 }
